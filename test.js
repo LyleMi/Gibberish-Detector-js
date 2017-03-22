e1bbce0b93c450d81a974a3e31dfa9b638e1f260
LyleMi <1278919492@qq.com>
2017-03-22 20:24:10 +0800

    update

--- test.js
+++ test.js
@@ -4,6 +4,23 @@ const fs = require('fs');
 const gib_detect = require('./gib_detect.js');
 let lines = fs.readFileSync('test.txt').toString('utf8').split('\n');
 
+function checkRate(nubmer) {
+    var re = /^[0-9a-zA-Z]*$/g;
+    if (!re.test(nubmer)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
 for (var i = 0; i < lines.length; i++) {
-    console.log(lines[i], gib_detect.gib_dect(lines[i]));
+
+    if(!checkRate(lines[i])) {
+        continue;
+    }
+
+    if (gib_detect.gib_dect(lines[i])) {
+        console.log(lines[i]);
+    }
+    // console.log(lines[i], gib_detect.gib_dect(lines[i]));
 }
